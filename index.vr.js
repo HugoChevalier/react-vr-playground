6ba6a76be0d0807bf407db97269f4322afedb2c4
hugo <hugo.chevalier@upsell.fr>
2017-05-18 12:47:22 +0000

    change app background

--- index.vr.js
+++ index.vr.js
@@ -11,10 +11,9 @@ export default class WelcomeToVR extends React.Component {
   render() {
     return (
       <View>
-        <Pano source={asset('chess-world.jpg')}/>
+        <Pano source={asset('wow.jpg')}/>
         <Text
           style={{
-            backgroundColor: '#777879',
             fontSize: 0.8,
             fontWeight: '400',
             layoutOrigin: [0.5, 0.5],
@@ -24,7 +23,7 @@ export default class WelcomeToVR extends React.Component {
             textAlignVertical: 'center',
             transform: [{translate: [0, 0, -3]}],
           }}>
-          hello
+          Such Wow
         </Text>
       </View>
     );
